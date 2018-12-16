6f1266a911799ba959fc274971ed51a3f4c39a76
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2018-12-16 13:10:17 +0100

    cpp files

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Ulrich Nilsson on 2018-12-13.
+//  Copyright © 2018 Ulrich Nilsson. All rights reserved.
+//
+/*
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf( "I am alive!  Beware.\n" );
+    getchar();
+    return 0;
+}
